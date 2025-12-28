5607278ecfd4916da44bba3c74c5954740c1172d
Kevin <krvoller@gmail.com>
2025-12-27 22:35:54 -0500

    Update fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,1 +1,15 @@
+import requests
 
+SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_66k9JHNj4a3At6WAhQ1tgiBtj4GoQdjmeqCL1nHty2aU2RRzviaN6O3_HJsQs2LkxMMiZBkYU86v/pub?output=csv"
+
+def download_data():
+    response = requests.get(SHEET_URL)
+    if response.status_code == 200:
+        with open("jentz_gig_dates_sheet_1.csv", "w") as f:
+            f.write(response.text)
+        print("Data updated successfully!")
+    else:
+        print("Failed to fetch data")
+
+if __name__ == "__main__":
+    download_data()
